96ef44a4e20525b80137c34e1eee84eeac29ac5b
Hellon Canella <helloncanella@gmail.com>
2016-07-21 14:45:45 -0300

    repport

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# require-user-problem
-Repo with require user's decorator problem
+# @RequireUser problem
+
+In `party-details.ts` the transpiler reports the package `{}/angular2-meteor-accounts-ui/build/index has no exported member RequireUser`.
+
+This repository contains a copy of the referred package.
---
+++ party-details.ts
@@ -0,0 +1,47 @@
+import { Component, NgZone  } from '@angular/core';
+import { ActivatedRoute, ROUTER_DIRECTIVES } from '@angular/router';
+import {Parties} from '../../../collections/parties.ts';
+import { Tracker } from 'meteor/tracker';
+import { Meteor } from 'meteor/meteor';
+import { RequireUser } from 'angular2-meteor-accounts-ui';
+
+import template from './party-details.html';
+
+@Component({
+  selector: 'party-details',
+  template,
+  directives: [ROUTER_DIRECTIVES]
+})
+
+@RequireUser()
+export class PartyDetails {
+  partyId: string;
+  party: Party;
+
+  constructor(private route: ActivatedRoute, private ngZone: NgZone) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.partyId = params['partyId'];
+      Tracker.autorun(() => {
+        this.ngZone.run(() => {
+          this.party = Parties.findOne(this.partyId);
+        });
+      });
+    });
+  }
+
+  saveParty(party) {
+    if (Meteor.userId()) {
+      Parties.update(party._id, {
+        $set: {
+          name: party.name,
+          description: party.description,
+          location: party.location
+        }
+      });
+    } else {
+      alert('Please log in to change this party');
+    }
+  }
+}
